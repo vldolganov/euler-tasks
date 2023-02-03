d740dd4374aef39aab18617285917f045802e9cb
vldolganov <v.dolganov@nv.dunice.net>
2023-02-03 10:38:42 +0300

    11

--- euler-10.js
+++ euler-10.js
@@ -1,25 +1,12 @@
-console.time('TIME')
-function solvedTask() {
-    const isPrime = x => {
-        if (x <= 3)
-            return true
-        if (x % 2 === 0)
-            return false
-        for (let i = 3; i <= Math.sqrt(x); i += 2) {
-            if (x % i === 0)
-                return false
-        }
 
-        return true
-    }
-    let sum = 0
-    for (let i = 2; i < 2000000; i++) {
-        if (isPrime(i))
-            sum += i
-    }
-    console.log(sum)
+
+function test() {
+    [[0, 1], [2, 3]].reduce(
+        (acc, cur) => {
+       console.log(acc.concat(cur));
+        },
+        [1, 2]
+    );
 }
 
-solvedTask()
-
-console.timeEnd('TIME')
+test()
