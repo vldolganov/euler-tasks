2196231605ad0aa28813a56b52c579bec4026170
Vladislav <v.dolganov@nv.dunice.net>
2023-05-15 09:21:45 +0300

    task 20

---
+++ euler-20.js
@@ -0,0 +1,18 @@
+console.time('TIME')
+function factorialDigitSum(n) {
+    let factorial = BigInt(1);
+    for (let i = 2; i <= n; i++) {
+      factorial *= BigInt(i);
+    }
+  
+    let digitSum = 0;
+    while (factorial > 0) {
+      digitSum += factorial % 10n;
+      factorial /= 10n;
+    }
+  
+    return digitSum;
+  }
+  
+  console.log(factorialDigitSum(100));
+  
